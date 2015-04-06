d5cf0136e81ae52f95eefdd8f3a9ce946744d948
svasyltc <stepan.vasilchenko@gmail.com>
2015-04-06 15:02:56 +0300

    PersonTable

--- lnu/personsTable/personsTableCtrl.js
+++ lnu/personsTable/personsTableCtrl.js
@@ -5,6 +5,16 @@ app.controller("PersonsTableCTRL",["$scope", function($scope){
 
         $scope.isShowGeneralInfo = true;
     };
+
 }]);/**
  * Created by Dmytro on 04.04.2015.
  */
+app.controller("personInfoCtrl", ["$scope", "$http", function($scope, $http){
+    $scope.tempPersonData = {};
+    $scope.getPersonData = function () {
+        $http.get('Content/tempData/tempData.json').success(function (data) {
+            $scope.tempPersonData = data;
+        });
+    };
+    $scope.getPersonData();
+}]);
