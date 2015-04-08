015a47fa9932200264d9ea36c7d3bc5af2c80591
svasyltc <webui054@gmail.com>
2015-04-09 01:14:31 +0300

    updated BegDate view in main info Tab and some other light edits.

--- lnu/personsTable/personsTableCtrl.js
+++ lnu/personsTable/personsTableCtrl.js
@@ -112,7 +112,7 @@ var personTabModal = angular.module('ui.bootstrap.demo', ['ui.bootstrap']);
 
 personTabModal.controller('ModalDemoCtrl', function ($scope, $modal, $log) {
 
-    $scope.open = function (name, persId) {
+    $scope.open = function (name, persId, index) {
         var modalInstance = $modal.open({
             templateUrl: 'myModalContent',
             controller: 'ModalInstanceCtrl',
@@ -122,12 +122,16 @@ personTabModal.controller('ModalDemoCtrl', function ($scope, $modal, $log) {
                      },
                     id: function () {
                         return persId;
+                    },
+                    currentIndex: function(){
+                        return index;
                     }
-            }
+                }
         });
 
         modalInstance.result.then(function (selectedItem) {
             $scope.selected = selectedItem;
+
         }, function () {
             $log.info('Modal dismissed at: ' + new Date());
         });
@@ -137,15 +141,14 @@ personTabModal.controller('ModalDemoCtrl', function ($scope, $modal, $log) {
 // Please note that $modalInstance represents a modal window (instance) dependency.
 // It is not the same as the $modal service used above.
 
-personTabModal.controller('ModalInstanceCtrl', function ($scope, $modalInstance, name, id) {
+personTabModal.controller('ModalInstanceCtrl', function ($scope, $modalInstance, name, id, currentIndex) {
 
     $scope.name = name;
     $scope.id = id;
-
+    $scope.currentIndex = currentIndex;
 
     $scope.ok = function () {
         $modalInstance.close(true);
-
     };
 
     $scope.cancel = function () {
@@ -153,3 +156,9 @@ personTabModal.controller('ModalInstanceCtrl', function ($scope, $modalInstance,
     };
 
 });
+
+/*
+$scope.removeRow = function(index) {
+    $scope.tempProposalArray.splice(index, 1);
+};
+*/
