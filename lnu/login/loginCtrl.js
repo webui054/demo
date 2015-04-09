92e67e590a2270bb07ca22cc8007ff13723deecf
dmirtokun@gmail.com <dmirtokun@gmail.com>
2015-04-09 19:35:25 +0300

    some project sceleton fix(remove View folder) remove some inline styles fix login page

--- lnu/login/loginCtrl.js
+++ lnu/login/loginCtrl.js
@@ -18,37 +18,37 @@ app.controller('LoginCtrl', ["$scope", "$http", "$filter", "$interval", function
         var tok = user + ':' + password;
         return btoa(tok);
     };
-    $scope.make_base_auth = function(token) {
+    $scope.makeBaseAuth = function(token) {
         return "Basic " + token;
     };
     var BASEURL = "http://104.236.29.16:8080/is-lnu-rest-api/";
 
-    //$scope.login = function(){
-    //    if($scope.userName === ""){
-    //        $('#userGroup').addClass('has-error');
-    //        return;
-    //    }
-    //    if( $scope.password === ""){
-    //        $('#pwrGroup').addClass('has-error');
-    //        return;
-    //    }
-    //    var token = $scope.makeToken($scope.userName,$scope.password);
-    //    var baseAuthString = $scope.make_base_auth(token);
-    //    $http.defaults.headers.common.Authorization = baseAuthString; // задаємо хедер запиту по замовчуванню для всіх типів крім POST і PUT
-    //    $http.get(BASEURL + "api/persons")
-    //        .success(function (data) {
-    //        $scope.$parent.isLoggedIn = true;
-    //        localStorage.setItem("token", token);
-    //        localStorage.setItem("baseAuthString", baseAuthString)})
-    //        .error(function (){
-    //            $('#userGroup').addClass('has-error');
-    //            $('#pwrGroup').addClass('has-error');
-    //        });
-    //};
-
     $scope.login = function(){
-        $scope.$parent.isLoggedIn = true;
+        if($scope.userName === ""){
+            $('#userGroup').addClass('has-error');
+            return;
+        }
+        if( $scope.password === ""){
+            $('#pwrGroup').addClass('has-error');
+            return;
+        }
+        var token = $scope.makeToken($scope.userName,$scope.password);
+        var baseAuthString = $scope.makeBaseAuth(token);
+        $http.defaults.headers.common.Authorization = baseAuthString; // задаємо хедер запиту по замовчуванню для всіх типів крім POST і PUT
+        $http.get(BASEURL + "api/persons")
+            .success(function (data) {
+            $scope.$parent.isLoggedIn = true;
+            localStorage.setItem("token", token);
+            localStorage.setItem("baseAuthString", baseAuthString)})
+            .error(function (){
+                $('#userGroup').addClass('has-error');
+                $('#pwrGroup').addClass('has-error');
+            });
     };
+
+    //$scope.login = function(){
+    //    $scope.$parent.isLoggedIn = true;
+    //};
 
 
 
