83efad0ebc2445def4d59ac21bc83c5e4a717d20
Nadia Novitskas <novitskas@gmail.com>
2015-04-10 14:26:58 +0300

    add select subject

--- lnu/proposals/subjects/subjectsCtrl.js
+++ lnu/proposals/subjects/subjectsCtrl.js
@@ -9,6 +9,27 @@ app.controller('subjectsCtrl', ["$scope", "$http", "$filter", "$interval", funct
     $scope.mark = "";
     $scope.weightSubject = "";
 
+    $scope.subjects = [
+        {name: "Українська мова", id: "1"},
+        {name: "Українська література", id: "2"},
+        {name: "Іноземна мова", id: "3"},
+        {name: "Світова література", id: "4"},
+        {name: "Історія України", id: "5"},
+        {name: "Всесвітня історія", id: "6"},
+        {name: "Правознавство", id: "7"},
+        {name: "Художня культура", id: "8"},
+        {name: "Математика", id: "9"},
+        {name: "Біологія", id: "10"},
+        {name: "Географія", id: "11"},
+        {name: "Психологія", id: "12"},
+        {name: "Фізика", id: "13"},
+        {name: "Хімія", id: "14"},
+        {name: "Екологія", id: "15"},
+        {name: "Інформатика", id: "16"},
+        {name: "Творчий конкурс", id: "17"},
+        {name: "Фаховий іспит", id: "18"}
+    ];
+
     $scope.showModal = function (id) {
         if (id === null) {
             return;
@@ -39,8 +60,9 @@ app.controller('subjectsCtrl', ["$scope", "$http", "$filter", "$interval", funct
         $("#addSubjectModal").modal("hide");
         $scope.tempData = {};
 
-        $scope.tempData.subjectName = $scope.subjectName;
-        $scope.tempData.enrolmentSubjectId = $scope.enrolmentSubjectId;
+        console.log($scope.subjectName);
+
+        $scope.tempData.subjectName = $scope.subjectName.name;
         $scope.tempData.specofferId = $scope.specofferId;
 
         if($scope.isMajor){ $scope.tempData.isMajor = "Так";
@@ -59,6 +81,7 @@ app.controller('subjectsCtrl', ["$scope", "$http", "$filter", "$interval", funct
             $scope.tempSubjectArray.splice(0,1)
         }
         $scope.tempSubjectArray.push($scope.tempData);
+
 
         $scope.subjectName = "";
         $scope.specofferId = "";
