01a61aa554dc40f9cbd7e35306bd8300830754b2
OksanaE <jessika19@meta.ua>
2015-04-20 18:19:16 +0300

    Deleted 'Zno' & 'Man' folders; added new .json to 'Mark' folder.

--- testrecurtion/app.js
+++ testrecurtion/app.js
@@ -1,33 +1,43 @@
 var main = angular.module('main', []);
-main.controller('Main',['$scope','$http', function($scope,$http){
-    $scope.dataArr= [];
+main.controller('Main', ['$scope', '$http', function ($scope, $http) {
+    $scope.dataArr = [];
 
-    $scope.makeToken =function(){
+    $scope.makeToken = function () {
         var tok = 'admin:nimda';
         return btoa(tok);
     };
-    $scope.makeBaseAuth = function(token) {
+    $scope.makeBaseAuth = function (token) {
         return "Basic " + token;
     };
     var BASEURL = "http://104.236.29.16:8080/is-lnu-rest-api/";
     //
 
-    var getData = function(offset){
-        return $http.get(BASEURL + "api/persons?limit=5&offset="+ offset)
-                .success(function (data) {
-                    angular.forEach(data.resources,function(elem){
-                        $scope.dataArr.push(elem);
-                    });
-                    if($scope.dataArr.length < data.count){
-                        offset += 5;
-                        getData(offset);
-                    }
-                })
-                .error(function (){
+    var getData = function (offset) {
+        return $http.get(BASEURL + "api/persons?limit=5&offset=" + offset)
+            .success(function (data) {
+                angular.forEach(data.resources, function (elem) {
+                    $scope.dataArr.push(elem);
                 });
+                if ($scope.dataArr.length < data.count) {
+                    offset += 5;
+                    getData(offset);
+                }
+            })
+            .error(function () {
+            });
     };
 
-    $scope.login = function(){
+    $scope.getZnoData = function () {
+
+        return $http.get(BASEURL + "api/papers/types/")
+            .success(function (data) {
+            })
+            .error(function () {
+            });
+    };
+
+
+    $scope.login = function () {
         var token = $scope.makeToken();
         var baseAuthString = $scope.makeBaseAuth(token);
         $http.defaults.headers.common['Authorization'] = baseAuthString;
@@ -36,8 +46,8 @@ main.controller('Main',['$scope','$http', function($scope,$http){
     };
     $scope.login();
 
-    $scope.addPerson = function(){
-        var url = BASEURL+" ";
+    $scope.addPerson = function () {
+        var url = BASEURL + " ";
 
         var data = {
             personTypeId: 1,
@@ -49,7 +59,7 @@ main.controller('Main',['$scope','$http', function($scope,$http){
             firstName: "Дмитро",
             surname: "Кузнєцов",
             fatherName: "Олександрович",
-            name: "Кузнєцов" +" "+ "Дмитро" +" "+ "Олександрович",
+            name: "Кузнєцов" + " " + "Дмитро" + " " + "Олександрович",
             identifier: "12345",
             isHostel: 1,
             isMilitary: 1,
@@ -60,22 +70,22 @@ main.controller('Main',['$scope','$http', function($scope,$http){
             docSeries: "СЕ"
         };
 
-        $http.post(url,data).success(function(data){
+        $http.post(url, data).success(function (data) {
             $scope.dataArr.push(data);
             console.log(data)
         })
     };
-    $scope.deletePerson = function(){
-        var url = BASEURL+"api/persons2/" + 12;
+    $scope.deletePerson = function () {
+        var url = BASEURL + "api/persons2/" + 12;
 
-        $http.delete(url).success(function(data){
+        $http.delete(url).success(function (data) {
 
             console.log(data)
-        }).error(function(err){
+        }).error(function (err) {
             console.log(err);
         })
     };
-    $scope.updatePerson = function(){
+    $scope.updatePerson = function () {
         var data = {
             personTypeId: 1,
             citizenCountryId: 1,
@@ -86,7 +96,7 @@ main.controller('Main',['$scope','$http', function($scope,$http){
             firstName: "Дмитро",
             surname: "Кузнєцов",
             fatherName: "Олександрович",
-            name: "Кузнєцов" +" "+ "Дмитро" +" "+ "Олександрович",
+            name: "Кузнєцов" + " " + "Дмитро" + " " + "Олександрович",
             identifier: "12345",
             isHostel: 1,
             isMilitary: 1,
@@ -96,9 +106,9 @@ main.controller('Main',['$scope','$http', function($scope,$http){
             docNum: "123456",
             docSeries: "СЕ"
         };
-        var url = BASEURL+"api/persons/" + 13;
+        var url = BASEURL + "api/persons/" + 13;
 
-        $http.put(url,data).success(function(data){
+        $http.put(url, data).success(function (data) {
 
             console.log(data)
         })
