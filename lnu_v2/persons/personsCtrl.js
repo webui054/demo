9ae9b5b53b56c43d3415fc28b38cbed43440fc0a
NadiaN <novitskas@gmail.com>
2015-04-24 10:14:42 +0300

    add full version of city/village selection, but still not perfect

--- lnu_v2/persons/personsCtrl.js
+++ lnu_v2/persons/personsCtrl.js
@@ -26,13 +26,17 @@ persons.controller("PersonsCtrl",["$scope","PersonRepo","$rootScope","$http","Pe
         $scope.addressData = {
             countries:[],
             regions:[],
-            asdTypes:[],
+            cdTypes:[],
             cities:[],
             districts:[],
-            villageType:[],
+            vType:[],
             villages:[],
             countryId:{},
-            regionId:{}
+            regionId:{},
+            cdTypeId:{},
+            districtId:{},
+            vTypeId:{},
+            villageId:{}
         }
 
         var baseUrl = "http://104.236.29.16:8080/is-lnu-rest-api/";
@@ -40,7 +44,7 @@ persons.controller("PersonsCtrl",["$scope","PersonRepo","$rootScope","$http","Pe
             $http.get(baseUrl + "api/adminunits?adminUnitTypeId=6").success(function(data){
                 var addEl = document.getElementById("addrsC");
                 addEl.addEventListener('change',function(){
-                    $scope.getChildAddressData($scope.addressData.countryId);
+                    $scope.getRegionAddressData($scope.addressData.countryId);
                 });
               $scope.addressData.countries = data.resources;
             });
@@ -49,10 +53,54 @@ persons.controller("PersonsCtrl",["$scope","PersonRepo","$rootScope","$http","Pe
         $scope.getCountries();
 
 
-        $scope.getChildAddressData = function(parentId){
+        $scope.getRegionAddressData = function(parentId){
             $http.get(baseUrl + "api/adminunits?parentId="+parentId).success(function(data){
+                var addEl = document.getElementById("addrsR");
+                addEl.addEventListener('change',function(){
+                    $scope.getCdTypesAddressData($scope.addressData.regionId);
+                });
                 $scope.addressData.regions = data.resources;
                 $scope.isCountrySelected = true;
+            });
+        };
+
+        $scope.getCdTypesAddressData = function(parentId){
+            $http.get(baseUrl + "api/adminunits?parentId="+parentId).success(function(data){
+                var addEl = document.getElementById("addrsCD");
+                addEl.addEventListener('change',function(){
+                    $scope.getDistrictAddressData($scope.addressData.cdTypeId);
+                });
+                $scope.addressData.cdTypes = data.resources;
+                $scope.isRegionSelected = true;
+            });
+        };
+
+        $scope.getDistrictAddressData = function(parentId){
+            $http.get(baseUrl + "api/adminunits?parentId="+parentId).success(function(data){
+                var addEl = document.getElementById("addrsD");
+                addEl.addEventListener('change',function(){
+                    $scope.getvTypeAddressData($scope.addressData.districtId);
+                });
+                $scope.addressData.districts = data.resources;
+                $scope.iscdTypeSelected = true;
+            });
+        };
+
+        $scope.getvTypeAddressData = function(parentId){
+            $http.get(baseUrl + "api/adminunits?parentId="+parentId).success(function(data){
+                var addEl = document.getElementById("addrsV");
+                addEl.addEventListener('change',function(){
+                    $scope.getVillageAddressData($scope.addressData.vTypeId);
+                });
+                $scope.addressData.vTypes = data.resources;
+                $scope.isDistrictSelected = true;
+            });
+        };
+
+        $scope.getVillageAddressData = function(parentId){
+            $http.get(baseUrl + "api/adminunits?parentId="+parentId).success(function(data){
+                $scope.addressData.villages = data.resources;
+                $scope.isvTypeSelected = true;
             });
         };
 
